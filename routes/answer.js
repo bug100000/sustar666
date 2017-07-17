43f8fd68d6fcbd3d8af5fd146a6dd26fcce096f3
tietoushaonian <bug100000@outlook.com>
2017-07-17 15:43:36 +0800

    创建了一个新项目

--- routes/answer.js
+++ routes/answer.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('answer', {err: "123"});
+  var state;
+  if (req.session.uid != undefined) {
+      state = true;
+  } else {
+      state = false;
+  };
+  res.render('answer', {state: state});
 });
 
 module.exports = router;
