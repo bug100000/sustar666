9e3826c956b717a0f41b1c3aad046e78cd71c56b
tietoushaonian <bug100000@outlook.com>
2017-07-18 09:07:49 +0800

    122

--- routes/tagQuestion.js
+++ routes/tagQuestion.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var router = express.Router();
 var tagdb = require('../db/tag.js');
+var questions = require('../db/questions.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  // console.log(req.query);
+  console.log(req.query);
   tagdb.tags.findOne({
       tag: req.query.tag
   }, function(err, doc) {
       if (err) console.log(err);
       if(doc){
-        if (req.session.uid != undefined) {
-          res.render('tagQuestion', {state: "true",tagName:doc.tag,describe:doc.describe});
-        }else {
-          res.render('tagQuestion', {state: "false",tagName:doc.tag,describe:doc.describe});
-        }
+          questions.qdb.find({
+            tags:req.query.tag
+          },function(err, data){
+            if(err) console.log(err);
+            if(data){
+              // console.log(data);
+              if (req.session.uid != undefined) {
+                res.render('tagQuestion', {state: "true",tagName:doc.tag,describe:doc.describe,ans:data});
+              }else {
+                res.render('tagQuestion', {state: "false",tagName:doc.tag,describe:doc.describe,ans:data});
+              }
+            }
+          });
       }
   });
 });
