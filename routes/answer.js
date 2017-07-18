93d29f3914482580e944faa7792836511724e82d
tietoushaonian <bug100000@outlook.com>
2017-07-18 09:57:54 +0800

    修改：     routes/answer.js# 	修改：     views/index.ejs

--- routes/answer.js
+++ routes/answer.js
@@ -3,16 +3,16 @@ var router = express.Router();
 var qdb = require('../db/questions.js')
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  console.log(req.query);
+router.get('/:id', function(req, res) {
+  var id = req.url.replace("/", "");
+  var id = id.replace("?", "");
   var state;
   if (req.session.uid != undefined) {
       state = true;
   } else {
       state = false;
   };
-  qdb.qdb.find({"_id": req.query.id}, function(err, data){
-    // console.log(data+"=======================================");
+  qdb.qdb.find({"_id": id}, function(err, data){
     res.render('answer', {state: state, data: data[0]});
   })
 });
