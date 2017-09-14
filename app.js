c38b03b272aa5a0fde95784e8baf4b4982fc65dc
tietoushaonian <bug100000@outlook.com>
2017-09-14 11:54:26 +0800

    123

--- app.js
+++ app.js
@@ -60,7 +60,10 @@ app.use(WebpackDevMiddleware(compiler, {
 app.use(WebpackHotMiddleware(compiler));
 
 app.use(logger('dev'));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
